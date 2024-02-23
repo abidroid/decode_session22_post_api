3f91a014ff61f98a6493c451e3b6ccbaf1aaea12
Abid <abidroid1@gmail.com>
2024-02-23 15:46:19 +0500

    screen done

--- lib/screens/create_post_screen.dart
+++ lib/screens/create_post_screen.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
 
-class CreatePostScreen extends StatelessWidget {
+class CreatePostScreen extends StatefulWidget {
   const CreatePostScreen({super.key});
+
+  @override
+  State<CreatePostScreen> createState() => _CreatePostScreenState();
+}
+
+class _CreatePostScreenState extends State<CreatePostScreen> {
+  TextEditingController titleC = TextEditingController();
+  TextEditingController bodyC = TextEditingController();
+
+  @override
+  void dispose() {
+    titleC.dispose();
+    bodyC.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,9 +24,38 @@ class CreatePostScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Submit Post'),
       ),
-      body: Column(children: [
-        
-      ],),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: titleC,
+              decoration: const InputDecoration(
+                hintText: 'Title',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            TextField(
+              controller: bodyC,
+              maxLines: 5,
+              decoration: const InputDecoration(
+                hintText: 'Body',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Submit Post'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
