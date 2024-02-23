23666a61e253a68c860bc1388f7192d65cbd1de0
Abid <abidroid1@gmail.com>
2024-02-23 15:28:53 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/pk/intellilogics/decode_session22_post_api/MainActivity.kt
@@ -0,0 +1,5 @@
+package pk.intellilogics.decode_session22_post_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/create_post_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CreatePostScreen extends StatelessWidget {
+  const CreatePostScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Submit Post'),
+      ),
+      body: Column(children: [
+        
+      ],),
+    );
+  }
+}
